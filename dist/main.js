7e98629439b40d2ad19d015a373513599e9879c8
Luiz-F-Brogiato <brogiatoluizfernando@gmail.com>
2025-04-15 09:52:17 -0300

    Aprendendo os tipos de dados com Typescript

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,20 @@
 "use strict";
+//Tipos de dados
+//String - texto, "", '', ``, 
+let nameUser = "João";
+let lastNameUser = "da Silva";
+let fullName = `${nameUser} ${lastNameUser}`;
+console.log(fullName);
+//Number - Inteiros, Decimais, Hexadecimais, Binários
+let age = 30;
+let statusUser = true;
+//Array - Variáveis indexadas. Vetores, Matrizes
+let fruits = ["Maça", "Banana", "Uva"];
+let fruits2 = ["Maça", "Banana", "Uva"];
+//Object - Objetos, propiedades e funcionalidades
+let company = {
+    name: 'Hcode',
+    address: "Avenida Jośe Versolato, 101",
+    city: "São José dos Campos",
+    state: "SP",
+};
---
+++ src/main.ts
@@ -0,0 +1,55 @@
+//Tipos de dados
+//String - texto, "", '', ``, 
+
+let nameUser: string = "João";
+let lastNameUser: string = "da Silva"
+let fullName: string = `${nameUser} ${lastNameUser}`;
+console.log(fullName)
+
+//Number - Inteiros, Decimais, Hexadecimais, Binários
+
+let age: number = 30;
+let statusUser: boolean = true;
+
+//Array - Variáveis indexadas. Vetores, Matrizes
+
+let fruits: Array<string> = ["Maça", "Banana", "Uva"]
+let fruits2: Array<string|number> = ["Maça", "Banana", "Uva"]
+
+//Object - Objetos, propiedades e funcionalidades
+
+type companyType = {
+    name: String,
+    adress: String,
+    city: String,
+    state?: String, //Faz ser opcional
+}
+
+let company: object = {
+    name: 'Hcode',
+    address: "Avenida Jośe Versolato, 101",
+    city: "São José dos Campos",
+    state: "SP",
+}
+
+//Date
+
+let bornDate = new Date('2022-01-01');
+
+//Tuple - Array com tipos de dados já definidos
+
+let address: [string, number] = ["Av. Paulista" , 1000]
+
+//Enum - lista de valores possíveis
+
+enum statusUserEnum  {
+    blocked = 0,
+    released = 1,
+    pending = 2
+}
+
+let options:statusUserEnum = statusUserEnum.pending
+
+//Any - qualquer tipo de dado, não recomendadado
+
+let obs:any = true;
