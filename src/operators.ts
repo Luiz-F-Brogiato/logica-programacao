0e1fd4401d43dc87fffc208bcb1219479d0f958e
Luiz-F-Brogiato <brogiatoluizfernando@gmail.com>
2025-04-25 16:36:44 -0300

    Operadores: de atribuição, aritiméticos e de comparação

--- src/operators.ts
+++ src/operators.ts
@@ -1,3 +1,39 @@
+//Operador de atribuição
 let nameCompany = "Hcode";
-
 console.log(nameCompany)
+
+
+//Operador aritmetico
+let price = 1500;
+let porcento = 0.1;
+ 
+console.log(Number(price * (1 + porcento)).toFixed(2));
+
+let startValue = 2;
+
+console.log(startValue ** 6);
+
+console.log(startValue % 2);
+
+//Operador de comparação
+
+if (price == 1000) {
+    console.log("Está caro");
+}
+
+if (price < 1000) {
+    console.log("Está barato");
+}
+
+if (price > 1000) {
+    console.log("Está caro ou caro demais");
+}
+
+if (price !== 1000) {
+    console.log("O valor é dirente de mil ou o tipo de dado é diferente");
+}
+/* 
+if (price === "100") {
+    console.log("Está caro");
+} === compara o tipo também  */
+
